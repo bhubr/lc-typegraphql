4cdf589ab1eb00450011e5d9a6d7bfbc01773545
Benoit Hubert <benoithubert@gmail.com>
2021-10-26 16:32:12 +0200

    Fetching data from MongoDB

--- src/resolver.ts
+++ src/resolver.ts
@@ -1,12 +1,14 @@
-import Wilder from './types/Wilder';
 import { Query, Resolver } from 'type-graphql';
+import Wilder from './types/Wilder';
+import WilderModel from './models/Wilder';
 
 @Resolver(Wilder)
 export default class WilderResolver {
 
   @Query(returns => [Wilder])
   // eslint-disable-next-line class-methods-use-this
-  allWilders() {
-    return [];
+  async allWilders() {
+    const wilders = await WilderModel.find();
+    return wilders;
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,38 @@
-// import mongoose from 'mongoose';
 import 'reflect-metadata';
+import mongoose from 'mongoose';
 import { ApolloServer } from 'apollo-server';
 import { buildSchema } from 'type-graphql';
 import WilderResolver from './resolver';
 
 async function startup() {
-  const schema = await buildSchema({
-    resolvers: [WilderResolver]
-  });
+  try {
+    await mongoose
+    .connect('mongodb://127.0.0.1:27017/wilderdb', {
+      autoIndex: true,
+    })
+    console.log('Connected to database');
+    // .then(() => console.log('Connected to database')) // eslint-disable-line no-console
+    // .catch((err) => console.log(err)); // eslint-disable-line no-console
 
-  const server = new ApolloServer({
-    schema,
-    // playground: true,
-  });
+    const schema = await buildSchema({
+      resolvers: [WilderResolver]
+    });
 
-  const port = process.env.PORT || 4000;
-  const { url } = await server.listen(port);
-  console.log(`Server is running, GraphQL Playground available at ${url}`);
+    const server = new ApolloServer({
+      schema,
+      // playground: true,
+    });
 
+    const port = process.env.PORT || 4000;
+    const { url } = await server.listen(port);
+    console.log(`Server is running, GraphQL Playground available at ${url}`);
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(`Could not start server because of error: ${err.message}`);
+    } else {
+      console.error(err);
+    }
+  }
 }
 
 startup();
-
-// // Database
-// mongoose
-//   .connect('mongodb://127.0.0.1:27017/wilderdb', {
-//     autoIndex: true,
-//   })
-//   .then(() => console.log('Connected to database')) // eslint-disable-line no-console
-//   .catch((err) => console.log(err)); // eslint-disable-line no-console
